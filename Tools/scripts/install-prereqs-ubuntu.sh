74e3e97c22b41d9457b1a9798b3943b5715c7848
gwen hastings <gwen@cypherpunks.to>
2014-03-14 20:40:41 -0700

    changes to grab the correct up to date arm compiler

--- Tools/scripts/install-prereqs-ubuntu.sh
+++ Tools/scripts/install-prereqs-ubuntu.sh
@@ -50,20 +50,23 @@ if [ ! -d ../PX4NuttX ]; then
     git clone https://github.com/diydrones/PX4NuttX.git
 fi
 
-if [ ! -d ~/gcc-arm-none-eabi-4_6-2012q2 ]; then
-    ARM_TARBALL=gcc-arm-none-eabi-4_6-2012q2-20120614.tar.bz2
+if [ ! -d ~/gcc-arm-none-eabi-4_8-2013-q4-major ]; then
+    #ARM_TARBALL=gcc-arm-none-eabi-4_8-2013-q4-major-20120614.tar.bz2
+    ARM_TARBALL=gcc-arm-none-eabi-4_8-2013q4-20131204-linux.tar.bz2
     (
         cd ~;
-        curl -OL "https://launchpad.net/gcc-arm-embedded/4.6/4.6-2012-q2-update/+download/$ARM_TARBALL";
+        #curl -OL "https://launchpad.net/gcc-arm-embedded/4.6/4.8-2013-q4-major/+download/$ARM_TARBALL";
+
+	curl -OL "https://launchpad.net/gcc-arm-embedded/4.8/4.8-2013-q4-major/+download/$ARM_TARBALL";
         tar xjf ${ARM_TARBALL};
         rm ${ARM_TARBALL};
     )
 fi
-exportline="export PATH=$HOME/gcc-arm-none-eabi-4_6-2012q2/bin:\$PATH";
+exportline="export PATH=$HOME/gcc-arm-none-eabi-4_8-2013-q4-major/bin:\$PATH";
 if ! grep -Fxq "$exportline" ~/.profile ; then
-    if maybe_prompt_user "Add $HOME/gcc-arm-none-eabi-4_6-2012q2/bin to your PATH [Y/n]?" ; then
+    if maybe_prompt_user "Add $HOME/gcc-arm-none-eabi-4_8-2013-q4-major/bin to your PATH [Y/n]?" ; then
         echo $exportline >> ~/.profile
     else
-        echo "Skipping adding $HOME/gcc-arm-none-eabi-4_6-2012q2/bin to PATH."
+        echo "Skipping adding $HOME/gcc-arm-none-eabi-4_8-2013-q4-major/bin to PATH."
     fi
 fi
